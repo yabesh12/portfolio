041b0ef0144c2a40e6a9d70e508342a44ac95c8f
yabesh <yabesh@gmail.com>
2022-09-03 21:54:03 +0530

    modified allowed hosts to accept all

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i#s+v5c%d5v$kr&e1twy8uv5k+m$p=#+2k#q&u-$3%pn)lr6b-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
